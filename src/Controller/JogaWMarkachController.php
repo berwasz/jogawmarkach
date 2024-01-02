cdb85f94715405f93ecbbc24318ad98b2e657278
bernard <bernard.waszczyk@gmail.com>
2024-01-02 13:49:05 +0100

    cache

--- src/Controller/JogaWMarkachController.php
+++ src/Controller/JogaWMarkachController.php
@@ -8,8 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class JogaWMarkachController extends AbstractController
 {
+
     #[Route('/', name: 'app_jogawmarkach_homepage')]
     public function homepage(): Response {
-        return $this->render('hp.html.twig');
+
+        $response = $this->render('hp.html.twig', []);
+
+        $response->setPublic();
+        $response->setMaxAge(2592000);
+
+        $response->headers->addCacheControlDirective('must-revalidate', true);
+
+        return $response;
     }
 }
